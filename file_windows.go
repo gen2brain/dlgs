1203fc6e12ae34d2558911fcd5d8380ccb9a3bab
SolarLune <solarlune@gmail.com>
2020-02-10 13:30:35 -0800

    Fix: Multiple extension filter wasn't working on MacOS for File() and FileMulti().Fix: Couldn't use same filter string across all platforms (as MacOS expects "jpg" instead of "*.jpg" for a filter string, unlike Windows and Linux). Now the characters are stripped down for Mac.

--- file_windows.go
+++ file_windows.go
@@ -9,7 +9,11 @@ import (
 	"unsafe"
 )
 
-// File displays a file dialog, returning the selected file/directory and a bool for success.
+// File displays a file dialog, returning the selected file or directory, a bool for success, and an
+// error if it was unable to display the dialog. Filter is a string that determines 
+// which extensions should be displayed for the dialog. Separate multiple file 
+// extensions by spaces and use "*.extension" format for cross-platform compatibility, e.g. "*.png *.jpg".
+// A blank string for the filter will display all file types.
 func File(title, filter string, directory bool) (string, bool, error) {
 	if directory {
 		out, ok := dirDialog(title)
@@ -20,7 +24,11 @@ func File(title, filter string, directory bool) (string, bool, error) {
 	return out, ok, nil
 }
 
-// FileMulti displays a file dialog, returning the selected files and a bool for success.
+// FileMulti displays a file dialog that allows for selecting multiple files. It returns the selected 
+// files, a bool for success, and an error if it was unable to display the dialog. Filter is a string 
+// that determines which files should be available for selection in the dialog. Separate multiple file 
+// extensions by spaces and use "*.extension" format for cross-platform compatibility, e.g. "*.png *.jpg".
+// A blank string for the filter will display all file types.
 func FileMulti(title, filter string) ([]string, bool, error) {
 	out, ok := fileDialog(title, filter, true)
 
