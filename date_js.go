f927eda844b95abd77fe5a9c9d834a4da252bcf7
Gábor Görzsöny <gabor@gorzsony.com>
2021-09-10 22:22:45 +0200

    Fixed js build errors

--- date_js.go
+++ date_js.go
@@ -3,10 +3,10 @@
 package dlgs
 
 import (
-//"github.com/gopherjs/gopherjs/js"
+	"time"
 )
 
 // Date displays a calendar dialog, returning the date and a bool for success.
 func Date(title, text string, defaultDate time.Time) (time.Time, bool, error) {
-	return nil, false, ErrNotImplemented
+	return time.Time{}, false, ErrNotImplemented
 }
